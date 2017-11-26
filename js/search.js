fbde41fc1cdddcf230dc040239f65126a6374ded
CN45 <32204984+CN45@users.noreply.github.com>
2017-11-26 13:57:08 -0800

    js files

---
+++ js/search.js
@@ -0,0 +1,15 @@
+$("#search").keyup(function() {
+  var current_query = $("#search").val().toUpperCase();
+
+  $(".gallery a").hide();
+
+  $(".gallery a").each(function() {
+
+    var current_keyword = $(this).attr("data-title");
+
+    if (current_keyword.toUpperCase().indexOf(current_query) >= 0) {
+      $(this).show();
+
+    }
+  });
+});
